0cd628d0d453cad744d191935e20b82ce7970da7
Marc Addeo <marcaddeo@gmail.com>
2024-09-28 13:05:49 -0400

    Update to latest pdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,46 @@
-use extism_pdk::{info, plugin_fn, FnResult, Prost};
-use hank_pdk::Hank;
-use hank_types::{database::PreparedStatement, message::Message, plugin::Metadata};
+use extism_pdk::{info, plugin_fn, FnResult};
+use hank_pdk::{Hank, PluginMetadata};
+use hank_types::database::PreparedStatement;
+use hank_types::message::Message;
 
 #[plugin_fn]
-pub fn get_metadata() -> FnResult<Prost<Metadata>> {
-    Ok(Prost(Metadata {
-        name: "sample-rust-plugin".into(),
-        description: "A sample plugin to demonstrate some functionality.".into(),
-        version: "0.1.0".into(),
-        database: true,
-    }))
+pub fn plugin() -> FnResult<()> {
+    let mut hank = Hank::new(PluginMetadata {
+        name: "sample-rust-plugin",
+        description: "A sample plugin to demonstrate some functionality.",
+        version: "0.1.0",
+        ..Default::default()
+    });
+
+    hank.register_install_handler(my_install);
+    hank.register_initialize_handler(my_initialize);
+    hank.register_message_handler(my_handle_message);
+    hank.register_command_handler(my_handle_command);
+
+    hank.start()
 }
 
-#[plugin_fn]
-pub fn install() -> FnResult<()> {
+pub fn my_install() {
     let stmt = PreparedStatement {
         sql: "CREATE TABLE IF NOT EXISTS people (name TEXT, age INTEGER)".into(),
         ..Default::default()
     };
     Hank::db_query(stmt);
-
-    Ok(())
 }
 
-#[plugin_fn]
-pub fn initialize() -> FnResult<()> {
+pub fn my_initialize() {
     info!("initializing...");
-
-    Ok(())
 }
 
-#[plugin_fn]
-pub fn handle_message(Prost(message): Prost<Message>) -> FnResult<()> {
+pub fn my_handle_message(message: Message) {
     info!("{}: {}", message.author_name, message.content);
-
-    Ok(())
 }
 
-#[plugin_fn]
-pub fn handle_command(Prost(message): Prost<Message>) -> FnResult<()> {
-    if message.content == "ping" {
+pub fn my_handle_command(command: Message) {
+    if command.content == "ping" {
         let response = Message {
             content: "Pong!".into(),
-            ..message
+            ..command
         };
         Hank::send_message(response);
     }
@@ -52,6 +50,4 @@ pub fn handle_command(Prost(message): Prost<Message>) -> FnResult<()> {
         ..Default::default()
     });
     info!("{:?}", people);
-
-    Ok(())
 }
